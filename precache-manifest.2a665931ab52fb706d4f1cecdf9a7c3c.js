1333965ba6699076faeec2df8d9df60c2ed108e8
AmnaMunawar461 <amnamunawar461@gmail.com>
2022-03-02 01:39:34 -0800

    Updates

---
+++ precache-manifest.2a665931ab52fb706d4f1cecdf9a7c3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6d3bf454741b28a18b7",
+    "url": "/ascDscGames/static/css/main.8b37c879.chunk.css"
+  },
+  {
+    "revision": "f6d3bf454741b28a18b7",
+    "url": "/ascDscGames/static/js/main.72072217.chunk.js"
+  },
+  {
+    "revision": "c99aee981e77ca41c1f2",
+    "url": "/ascDscGames/static/js/runtime~main.b43dfd8c.js"
+  },
+  {
+    "revision": "6dd04b30533a8ca6d8df",
+    "url": "/ascDscGames/static/css/2.3963232a.chunk.css"
+  },
+  {
+    "revision": "6dd04b30533a8ca6d8df",
+    "url": "/ascDscGames/static/js/2.50190508.chunk.js"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/ascDscGames/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/ascDscGames/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/ascDscGames/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/ascDscGames/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/ascDscGames/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "efc0e3314bf0a9623a140aa5e147db9f",
+    "url": "/ascDscGames/static/media/background.efc0e331.gif"
+  },
+  {
+    "revision": "669f8cdb6aa1730b188e1d564f50d648",
+    "url": "/ascDscGames/static/media/bar.669f8cdb.png"
+  },
+  {
+    "revision": "248a021df77d5b8106b0379d94c12e7d",
+    "url": "/ascDscGames/index.html"
+  }
+];
